b4b01646dde325ccd88fd0419986dce8e213c620
Max <zixel37@gmail.com>
2019-06-16 01:46:29 +0300

    exercise2

--- script.js
+++ script.js
@@ -7,24 +7,52 @@ let money,
     income = [];
 money = prompt("Ваш бюджет на месяц?");
 time = prompt("Введите дату в формате YYYY-MM-DD");
-time = new Date(time);
+//time = new Date(time);
 
-    expences.a1 = prompt("Введите обязательную статью расходов на этот месяц");
-    expences.a2 = prompt("Во сколько обойдется?");
-    expences.a3 = prompt("Введите обязательную статью расходов на этот месяц");
-    expences.a4 = prompt("Во сколько обойдется?");
+// expences.a1 = prompt("Введите обязательную статью расходов на этот месяц");
+// expences.a2 = prompt("Во сколько обойдется?");
+// expences.a3 = prompt("Введите обязательную статью расходов на этот месяц");
+// expences.a4 = prompt("Во сколько обойдется?");
+
+for (let index = 0; index < 2; index++) {
+    let a = prompt("Введите обязательную статью расходов на этот месяц"),
+        b = prompt("Во сколько обойдется?");
+    if (typeof (a) === 'string' && typeof (a) != null && a != '' && typeof (b) === 'string' &&
+        typeof (b) != null && b != '') {
+        expences[a] = b;
+    } else {
+        index--;
+    }
+
+}
+// let index = 0;
+// while (index < 2) {
+
+//     let a = prompt("Введите обязательную статью расходов на этот месяц"),
+//         b = prompt("Во сколько обойдется?");
+//     if (typeof (a) === 'string' && typeof (a) != null && a != '' && typeof (b) === 'string' &&
+//         typeof (b) != null && b != '') {
+//         expences[a] = b;
+//         index++;
+//     }
+
+// }
+// let index = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов на этот месяц"),
+//         b = prompt("Во сколько обойдется?");
+//     if (typeof (a) === 'string' && typeof (a) != null && a != '' && typeof (b) === 'string' &&
+//         typeof (b) != null && b != '') {
+//         expences[a] = b;
+//         index++;
+//     }
+// } while (index < 2);
+
 
 appdata.time = time;
 appdata.money = money;
 appdata.expences = expences;
 appdata.optionalExpenses = optionalExpenses;
 appdata.savings = false;
-let result = (money - expences.a2 - expences.a4) / 30;
-if (result <= 0 ) {
-alert("Свободных средств нет");  
-} else {
-alert("Бюджет на 1 день составляет: " + result.toFixed(2));
-}
-
-//let answer = confirm("Are yoe here?");
-//console.log(answer);
+appdata.moneyPerDay = appdata.money / 30;
+alert(appdata.moneyPerDay);
