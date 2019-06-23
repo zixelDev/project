d9f7251f0abb1a39d7c79c84498ce2306da58b9c
Max <zixel37@gmail.com>
2019-06-23 20:41:32 +0300

    exercise4

--- script.js
+++ script.js
@@ -1,109 +1,89 @@
 'use strict';
-let money, time, appdata = {},
-    expences = {},
-    optionalExpenses = {},
-    income = [];
+let money, time;
 
- function start() {
-     money = prompt("Ваш бюджет на месяц?");
-     time = prompt("Введите дату в формате YYYY-MM-DD");
-     while (isNaN(money) || money == '' || money == null) {
-         money = prompt("Ваш бюджет на месяц?");
-     }
- }
- start();
+function start() {
+    money = prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+    while (isNaN(money) || money == '' || money == null) {
+        money = prompt("Ваш бюджет на месяц?");
+    }
+}
+start();
 
-function ChooseExpences() {
-    for (let index = 0; index < 2; index++) {
-        let a = prompt("Введите обязательную статью расходов на этот месяц"),
-            b = prompt("Во сколько обойдется?");
-        if (typeof (a) === 'string' && typeof (a) != null && a != '' &&
-            typeof (b) === 'string' && typeof (b) != null && b != '') {
-            expences[a] = b;
+let appdata = {
+    timedata: time,
+    budget: money,
+    expences: {},
+    optionalExpenses: {},
+    savings: true,
+    income: [],
+    ChooseExpences: function () {
+        for (let index = 0; index < 2; index++) {
+            let a = prompt("Введите обязательную статью расходов на этот месяц"),
+                b = prompt("Во сколько обойдется?");
+            if (typeof (a) === 'string' && typeof (a) != null && a != '' &&
+                typeof (b) === 'string' && typeof (b) != null && b != '') {
+                appdata.expences[a] = b;
+            } else {
+                index--;
+            }
+
+        }
+    },
+    detectDayBudget: function () {
+        appdata.moneyPerDay = (appdata.budget / 30).toFixed(2);
+        alert(appdata.moneyPerDay);
+    },
+    DetectLevel: function () {
+        if (appdata.moneyPerDay < 100) {
+            console.log("Это минимальный уровень достатка!");
+        } else if (appdata.moneyPerDay > 100 && appdata.moneyPerDay < 2000) {
+            console.log("Это средний уровень достатка!");
+        } else if (appdata.moneyPerDay > 2000) {
+            console.log("Это высокий уровень достатка!");
         } else {
-            index--;
+            console.log("Произошла ошибка");
         }
 
-    }
-}
-ChooseExpences();
+    },
+    CheckSavings: function () {
+        let save, percent;
+        if (appdata.savings == true) {
+            save = +prompt("Каков ваш бюджет?");
+            percent = +prompt("Под какой процент?");
+        }
+        appdata.MothInCome = save / 100 * percent / 12;
+        alert("Доход с вашего депозита в месяц: " + appdata.MothInCome);
+    },
+    chooseOptExpenses: function () {
+        let a, index;
+        index = 1;
+        while (index < 4) {
+            a = prompt("Введите статью необязательных расходов");
+            if (typeof (a) === 'string' && typeof (a) != null && a != '') {
+                appdata.optionalExpenses[index] = a;
+                index++;
+            }
+        }
+    },
+    chooseInCome: function () {
+        let ans = prompt("Что вам может принести доход (Перечислите через запятую)?");
 
-appdata.time = time;
-appdata.money = money;
-appdata.expences = expences;
-appdata.optionalExpenses = optionalExpenses;
-appdata.savings = true;
-appdata.moneyPerDay = (appdata.money / 30).toFixed(2);
-alert(appdata.moneyPerDay);
-
-function DetectLevel() {
-    if (appdata.moneyPerDay < 100) {
-        console.log("Это минимальный уровень достатка!");
-    } else if (appdata.moneyPerDay > 100 && appdata.moneyPerDay < 2000) {
-        console.log("Это средний уровень достатка!");
-    } else if (appdata.moneyPerDay > 2000) {
-        console.log("Это высокий уровень достатка!");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-DetectLevel();
-
-
-function CheckSavings() {
-    let save, percent;
-    if (appdata.savings == true) {
-        save = +prompt("Каков ваш бюджет?");
-        percent = +prompt("Под какой процент?");
-    }
-    appdata.MothInCome = save / 100 * percent / 12;
-    alert("Доход с вашего депозита в месяц: " + appdata.MothInCome);
-}
-
-CheckSavings();
-
-
-function detectDayBudget() {
-    let DayInCome;
-    DayInCome = appdata.MothInCome / 30;
-    alert(DayInCome);
-}
-detectDayBudget();
-
-
-function chooseOptExpenses() {
-    let a, index;
-    index = 1;
-    while (index < 4) {
-        a = prompt("Введите статью необязательных расходов");
-        if (typeof (a) === 'string' && typeof (a) != null && a != '') {
-            appdata.optionalExpenses[index] = a;
-            index++;
+        while (ans == '' || ans == null || typeof (ans) != 'string' || !isNaN(Number(ans))) {
+            ans = prompt("Что вам может принести доход (Перечислите через запятую)?");
+        }
+        appdata.income = ans.split(", ");
+        appdata.income.push(prompt("Может что-то ещё?"));
+        appdata.income.sort();
+        console.log("Способы доп.заработка: ");
+        appdata.income.forEach(function (item, i) {
+            if (i > 0) {
+                console.log(item);
+            }
+        });
+        console.log("Наша программа включает в себя:");
+        for (let key in appdata) {
+           console.log( "Свойство: " + key + ", значение: " +appdata[key]); 
         }
     }
-}
-chooseOptExpenses();
-
-
-// let index = 0;
-// while (index < 2) {
-
-//     let a = prompt("Введите обязательную статью расходов на этот месяц"),
-//         b = prompt("Во сколько обойдется?");
-//     if (typeof (a) === 'string' && typeof (a) != null && a != '' && typeof (b) === 'string' &&
-//         typeof (b) != null && b != '') {
-//         expences[a] = b;
-//         index++;
-//     }
-
-// }
-// let index = 0;
-// do {
-//     let a = prompt("Введите обязательную статью расходов на этот месяц"),
-//         b = prompt("Во сколько обойдется?");
-//     if (typeof (a) === 'string' && typeof (a) != null && a != '' && typeof (b) === 'string' &&
-//         typeof (b) != null && b != '') {
-//         expences[a] = b;
-//         index++;
-//     }
-// } while (index < 2);
+};
